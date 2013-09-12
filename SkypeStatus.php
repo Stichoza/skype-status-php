153aac1d31de6e9da5a3236eea18c67fbccd34a7
Levan Velijanashvili <stichoza@gmail.com>
2013-09-12 05:46:21 -0700

    Create SkypeStatus.php

---
+++ SkypeStatus.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+*  @name:      Get user's Skype online status
+*  @author:    Levan Velijanashvili <admin@stichoza.com>
+*  @version:   1.0
+*/
+
+class SkypeStatus {
+
+    private $languageArray = Array(
+        "Unknown",
+        "Offline",
+        "Online",
+        "Away",
+        "Unavailable",
+        "Do Not Disturb",
+        "Invisible",
+        "SkypeME");
+
+    protected $xmlObject;
+
+    public function __construct($username) {
+        $xmlString = @file_get_contents("http://mystatus.skype.com/" . $username .
+            ".xml");
+        return ($xmlString) ? $this->xmlObject = new SimpleXMLElement($xmlString) : null;
+    }
+
+    private function checkObject() {
+        return ($this->xmlObject !== null);
+    }
+
+    public function getStatusCode() {
+        return ($this->checkObject()) ? $this->xmlObject->Status->presence[0] : null;
+    }
+
+    public function getStatusString() {
+        return ($this->checkObject()) ? $this->languageArray[intval($this->
+            getStatusCode())] : null;
+    }
+
+}
+
+$mySkype = new SkypeStatus($_REQUEST["u"]);
+echo $mySkype->getStatusString();
+
+?>
